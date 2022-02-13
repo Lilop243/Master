5d1214d76a4d62f7048aaf9f99c8265075571703
lida lopez <linda.lucia15@hotmail.com>
2022-02-13 15:58:25 -0500

    commit

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -4,12 +4,12 @@ import 'firebase/auth';
 import {seedDatabase} from '../seed';
 
 const config = {
-  apiKey: "AIzaSyBZi9No5F1otng4N56ERnuTfAWzJQGaaZA",
-  authDomain: "prueba-a6549.firebaseapp.com",
-  projectId: "prueba-a6549",
-  storageBucket: "prueba-a6549.appspot.com",
-  messagingSenderId: "798198946551",
-  appId: "1:798198946551:web:96f4fceb8eebaf5e9711f2"
+  apiKey: "AIzaSyCwfFuj8erx21h1W7bUvlPLl8dUUmrGxW8",
+  authDomain: "masn-adf14.firebaseapp.com",
+  projectId: "masn-adf14",
+  storageBucket: "masn-adf14.appspot.com",
+  messagingSenderId: "248753456223",
+  appId: "1:248753456223:web:784cda0294025072e347ea"
 };
 
 const firebase = Firebase.initializeApp(config);
