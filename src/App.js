65c0574d4ee0bacc2f3438abbbe9bb6d8dcdfc31
lida lopez <linda.lucia15@hotmail.com>
2022-02-21 19:53:45 -0500

    nuev

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Home, Browse, SignIn, SignUp } from './pages';
 import * as ROUTES from './constants/routes';
 import { IsUserRedirect, ProtectedRoute } from './helpers/routes';
 import { useAuthListener } from './hooks';
-import Apppp from './pages/Apppp';
+// import Apppp from './pages/Apppp';
 
 export function App() {
   const { user } = useAuthListener();
@@ -16,9 +16,9 @@ export function App() {
           <SignIn />
         </IsUserRedirect>
 
-        <IsUserRedirect user={user}  path={ROUTES.CRUD}>
+        {/* <IsUserRedirect user={user}  path={ROUTES.CRUD}>
           <Apppp />
-        </IsUserRedirect>
+        </IsUserRedirect> */}
 
         <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_UP}>
           <SignUp />
